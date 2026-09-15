410c030f4e3bbec172ba8b974d81491dd27055a5
Onoyri Mokube ESeokwea <onoyrimokube@gmail.com>
2026-09-15 13:47:26 -0600

    CHANGE

---
+++ Lesson04Methods/Warmup.cs
@@ -0,0 +1,14 @@
+class program
+{
+    0 references
+    static void Main()
+    {
+        Console.WriteLine(square(5));
+    }
+    1 references
+    static int  Square(int n)
+    {
+        return n * n;
+    }
+
+}
