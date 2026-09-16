8e34dce935cb8df663bacc4b36ad4aab6ae27f69
Onoyri Mokube ESeokwea <onoyrimokube@gmail.com>
2026-09-16 14:46:13 -0600

    change

--- Lesson06Objects/BookDemo.cs
+++ Lesson06Objects/BookDemo.cs
@@ -1,22 +1,35 @@
 // Book book1 = new Book();
-Book book1 =  new Book("The Hobbit", "J.R.R. Tolkien");
+// Book book1 =  new Book("The Hobbit", "J.R.R. Tolkien");
 // book1.Title = "The Hobbit";
 // book1.Author = "J.R.R Tolken";
-Console.WriteLine(book1.Title);
+// Console.WriteLine(book1.Title)
+
+Book book2 = new Book("Dune", "Frank Herbert");
+book1.Title = "The Hobbit - Updated";
+book1.pages = 555;
+book1.PrintSummary();
+book2.PrintSummary();
 class Book
 {
+      // public int pages; // field
+    public int Pages { get; set; }  // auto-implemented property
     public string Title{ get; set; } = "";
     public string Author{ get; set; } = "";
-
-    public Book( string title, string author)
+   
+   
+    
+    public Book( string title, string author, int pages = 0)
 
     {
         this.Title = title;
         this.Author = author;
+        this.Pages = pages;
     }
     public void PrintSummary()
 
     {
         Console.WriteLine($"{Title} by {Author}");
     }
+    
+    
 }
---
+++ Lesson06Objects/ReferenceCheck.cs
@@ -0,0 +1,11 @@
+using System.Collections.Concurrent;
+
+Book first = new Book("Original");
+Book alias = first;
+alias.Title = "Changed through alias";
+Console.WriteLine(first.Title);
+class Book
+{
+    public string Title {get; set;}
+    public Book(string title ) {Title = title; }
+}
